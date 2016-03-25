ee31a9039d352e954d709c7480471a204b995c1e
CHOISY Tommy <choisy.tommy@gmail.com>
2016-03-25 22:53:50 +0100

    improve plugin add tl and sl

--- index.js
+++ index.js
@@ -7,44 +7,106 @@ module.exports = (pluginContext) => {
     const shell = pluginContext.shell;
     const toast = pluginContext.toast;
 
- 
-
+    const args = [
+        {"argument" : "-sl" },
+        {"argument" : "-tl" },
+        {"argument" : "-m" },
+    ];
 
     function search(query, res) {
         const query_trim = query.trim();
         if (query_trim.length == 0) {
             return;
         }
+
+        var queryParse = parseQuery(query_trim);
         res.add({
-            id: query_trim,
+            id: queryParse,
             payload: 'translate',
-            title: 'Search ' + query_trim,
-            desc: 'Translate your sentence EN => FR'
+            title: 'Translate ' + ((getByArg("m", queryParse) != null)?getByArg("m", queryParse): "your sentence"),
+            desc: 'Translate your sentence',
+            icon: "#fa fa-language",
         });
+        
     }
 
     function execute(id, payload) {
         
         if (payload == 'translate') {
-            var sourceLang = 'en';
-            var targetLang = 'fr';
-            var sourceText = id;
-             var url = "https://translate.googleapis.com/translate_a/single?client=t&sl=" 
+            var sourceLang = ((getByArg("sl", id) != null)?getByArg("sl", id): "en");
+            var targetLang = ((getByArg("tl", id) != null)?getByArg("tl", id): "fr");
+            var sourceText = ((getByArg("m", id) != null)?getByArg("m", id): "");
+            
+
+            if(sourceText != ""){
+                var url = "https://translate.googleapis.com/translate_a/single?client=t&sl=" 
                     + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
 
-            got(url)
+                got(url)
                 .then(response => {
                     var result  = response.body;
                     result = result.substr(4);
                     toast.enqueue(result.substr(0, result.indexOf('"')), 2500);
                 })
                 .catch(error => {
-                    toast.enqueue("error", 2500);
+                    toast.enqueue("An error has occurred", 2500);
+                    // toast.enqueue(error.response.body, 2500);
                 });
+            }else{
+                toast.enqueue("You must write a sentence to translate", 2500);
+            }
+    
         }else{
             return;
         }
     }
 
+    function parseQuery(query){
+        var obj = [];
+        for(var i=0; i < args.length; i++){
+            var pos = query.indexOf(args[i].argument);
+            if(pos >= 0) {
+                obj.push( {"arg": args[i].argument, "value" : getValue(args[i].argument, query) } );
+            }
+        }
+        if(obj.length == 0){
+            obj.push({"arg": "-m", "value" : query });
+        }
+        return obj; 
+    }
+
+    function getValue(arg, query){
+        var pos = query.indexOf(arg);
+        if(pos != 0){
+            query = query.substr(pos, query.length);
+        }
+        
+        query = query.substr(arg.length, query.length);
+        query = query.substr(0, posNextArg(query));
+        
+        return query.trim();
+    }
+
+    function posNextArg(query){
+        var minPos = query.length;
+        for(var i=0; i < args.length; i++){
+            var pos = query.indexOf(args[i].argument);
+            if(pos >= 0) {
+                if(pos < minPos){
+                    minPos = pos;
+                }
+            }
+        }
+        return minPos;
+    }
+
+    function getByArg(arg, parse){
+        for(var i=0; i < parse.length; i++){
+            if(parse[i].arg == "-"+arg){
+                return parse[i].value;
+            }
+        }
+        return null;
+    }
     return {search, execute};
 };
--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
-TRANSLATE
+<h1 align="center">
+	Hain Translate 
+</h1>
+
+
+## Usage
+
+```
+/translate -m your_sentence -tl target_language -sl source_language
+```
+
+
+
+## Related
+
+- [got](https://github.com/sindresorhus/got) - Convenience wrapper for interacting with the API
+
+
+
+## License
+
+MIT © [Tommy CHOISY](https://github.com/tchoisy)
